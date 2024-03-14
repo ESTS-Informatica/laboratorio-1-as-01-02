4cb97aadb41c428f5f4cbd1e2a54aec696c993aa
filipeparedes <202300257@estudantes.ips.pt>
2024-03-14 10:37:48 +0000

    Classe

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -4,10 +4,29 @@ public class WordGuessingGame
 {
     InputReader reader;
 
-
-
-    private void play()
+    public void play()
     {
+        showWelcome();
+    }
+
+    private void showWelcome()
+    {
+        System.out.println("-----------------------------\n\n");
+        System.out.println("Welcome to word guessing game\n");
+        System.out.println("Press enter to continue\n\n");
+        System.out.println("-----------------------------\n\n");
+        Scanner(system.in).nextLine();
+
+    }
+
+    private void guess()
+    {
+
+    }
+
+    private void showResult()
+    {
+
 
     }
 }
