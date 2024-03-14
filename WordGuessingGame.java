5496c1e1d9b1217c95cb7f33b8579fff5df1779e
Miguel <202300322@estudantes.ips.pt>
2024-03-14 10:38:40 +0000

    teste

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -2,9 +2,41 @@ import java.util.Scanner;
 
 public class WordGuessingGame
 {
-    InputReader reader;
+    private InputReader reader;
+    private String hiddenWord;
+    private String guessedWord;
+    private int numberOfTries;
 
+    //construtor da classe
+    public WordGuessingGame(){
+        this.hiddenWord = "";
+        this.guessedWord = "";
+        this.numberOfTries = 0;
+    }
 
+    /*Selectores e Modificadores das variáveis*/
+    public String getHiddenWord() {
+        return this.hiddenWord;
+    }
+    public String getGuessedWord() {
+        return this.guessedWord;
+    }
+
+    public int getNumberOfTries(){
+        return this.numberOfTries;
+    }
+
+    public void setHiddenWord(String newHiddenWord){
+        this.hiddenWord = newHiddenWord;
+    }
+
+    public void setGuessedWord(String newGuessedWord){
+        this.guessedWord = newGuessedWord;
+    }
+
+    public void setNumberOfTries(int newNumberOfTries){
+        this.numberOfTries  = newNumberOfTries;
+    }
 
     private void play()
     {
