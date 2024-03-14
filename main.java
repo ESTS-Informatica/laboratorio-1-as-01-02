6d2b69e1ec7fdda68dc0d3e900a7aacc2548e97f
filipeparedes <202300257@estudantes.ips.pt>
2024-03-14 11:00:02 +0000

    Classe

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main
+{
+    private WordGuessingGame game;
+
+    public static void main(String[] args) {
+        game = new WordGuessingGame();
+
+        game.play();
+    }
+
+}
